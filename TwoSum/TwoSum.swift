7f8d14bb7a85b9c30fee5eb3067901c525c8340e
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-08-09 13:01:01 +0530

    added code and test for that

---
+++ TwoSum/TwoSum.swift
@@ -0,0 +1,21 @@
+//
+//  TwoSum.swift
+//  TwoSum
+//
+//  Created by Prashant Dwivedi on 09/08/21.
+//
+
+import Foundation
+public final class TwoSum {
+
+    let name = "TwoSum"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
